cb1f5b0b2513af0bd945ca6eb0d9111f5b1d6212
JustinSsu <ssu.pei.je@gmail.com>
2024-09-04 11:44:44 +0800

    create hw3 and copy hw2 and refactor code and test backend and add depend

---
+++ HomeworkThree/src/main/java/homeworkthree/HomeworkThreeApplication.java
@@ -0,0 +1,13 @@
+package homeworkthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkThreeApplication.class, args);
+    }
+
+}
