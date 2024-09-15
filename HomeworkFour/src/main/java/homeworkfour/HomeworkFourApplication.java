2960b62925c5ffac6a9ca41dad06f44abbcb1520
JustinSsu <ssu.pei.je@gmail.com>
2024-09-15 22:28:28 +0800

    add timeout time and change file name

--- HomeworkFour/src/main/java/homeworkfour/HomeworkFourApplication.java
+++ HomeworkFour/src/main/java/homeworkfour/HomeworkFourApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeworkThreeApplication {
+public class HomeworkFourApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HomeworkThreeApplication.class, args);
+        SpringApplication.run(HomeworkFourApplication.class, args);
     }
 
 }
